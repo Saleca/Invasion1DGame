5f4b56f0504df622b76360979c7338996b4a4d8a
Gonçalo <70868465+Saleca@users.noreply.github.com>
2024-08-25 12:20:04 +0100

    fixed weave ui issue

--- Invasion1D/Models/Character.cs
+++ Invasion1D/Models/Character.cs
@@ -3,108 +3,113 @@ using Timer = System.Timers.Timer;
 
 namespace Invasion1D.Models
 {
-	public abstract class Character : Kinetic
-	{
-		public float
-			health = 1,
-			vitalux = 1,
-			vitaAttackCost = 0.25f,
-			weaveAttackCost = 0.50f;
+    public abstract class Character : Kinetic
+    {
+        public float
+            health = 1,
+            vitalux = 1,
+            vitaAttackCost = 0.25f,
+            weaveAttackCost = 0.50f;
 
-		public int
-			warpium = 1;
+        public int
+            warpium = 1;
 
-		public bool
-			weave = false;
+        public bool
+            weave = false;
 
-		float weaveCooldownProgress = 1;
-		public Timer weaveCooldownTimer = null!;
+        float weaveCooldownProgress = 1;
+        public Timer weaveCooldownTimer = null!;
 
-		public Character(Dimension dimension, float position, Color color, float speed) : base(dimension, position, color, speed)
-		{
-			weaveCooldownTimer = SetUpTimer(Stats.smoothIncrementIntervalMS, OnWeaveCooldownElapsed, true);
-		}
+        public Character(Dimension dimension, float position, Color color, float speed) : base(dimension, position, color, speed)
+        {
+            weaveCooldownTimer = SetUpTimer(Stats.smoothIncrementIntervalMS, OnWeaveCooldownElapsed, true);
+        }
 
-		//temporary
-		public void EndWeaveCooldown(bool weave) => this.weave = weave;
+        //temporary
+        public void EndWeaveCooldown(bool weave) => this.weave = weave;
 
-		/// <summary>
-		/// uses vitalux to inflict damage
-		/// </summary>
-		public abstract void Attack();
+        /// <summary>
+        /// uses vitalux to inflict damage
+        /// </summary>
+        public abstract void Attack();
 
-		public void AddVitalux(float amount, out float remaining)
-		{
-			remaining = amount;
-			if (vitalux >= 1) return;
+        public void AddVitalux(float amount, out float remaining)
+        {
+            remaining = amount;
+            if (vitalux >= 1) return;
 
-			vitalux += amount;
-			if (vitalux > 1)
-			{
-				remaining = vitalux - 1;
-				vitalux = 1;
-			}
-			else
-			{
-				remaining = 0;
-			}
-		}
+            vitalux += amount;
+            if (vitalux > 1)
+            {
+                remaining = vitalux - 1;
+                vitalux = 1;
+            }
+            else
+            {
+                remaining = 0;
+            }
+        }
 
-		public void AddHealth(float amount, out float remaining)
-		{
-			remaining = amount;
-			if (health >= 1) return;
+        public void AddHealth(float amount, out float remaining)
+        {
+            remaining = amount;
+            if (health >= 1) return;
 
-			health += amount;
-			if (health > 1)
-			{
-				remaining = health - 1;
-				health = 1;
-			}
-			else
-			{
-				remaining = 0;
-			}
-		}
+            health += amount;
+            if (health > 1)
+            {
+                remaining = health - 1;
+                health = 1;
+            }
+            else
+            {
+                remaining = 0;
+            }
+        }
 
-		public void AddWarpium() => warpium++;
+        public void AddWarpium() => warpium++;
 
-		public bool AddWeave()
-		{
-			if (weave)
-				return false;
+        public bool AddWeave()
+        {
+            if (weave)
+                return false;
 
-			vitalux = 1;
-			weave = true;
+            vitalux = 1;
+            weave = true;
 
-			ActivateWeaveCooldown();
+            ActivateWeaveCooldown();
 
-			return true;
-		}
+            return true;
+        }
 
-		public void ActivateWeaveCooldown()
-		{
-			weaveCooldownProgress = 1;
-			Game.UI.RunOnUIThread(() => Game.UI.UpdateWeave(weaveCooldownProgress));
-			weaveCooldownTimer?.Start();
-		}
-		protected void OnWeaveCooldownElapsed(object? sender, EventArgs e)
-		{
-			weaveCooldownProgress -= Stats.weaveCoolDownIncrement;
-			Game.UI.RunOnUIThread(() =>
-			{
-				if (disposed)
-				{
-					return;
-				}
-				Game.UI.UpdateWeave(weaveCooldownProgress);
-			});
-
-			if (weaveCooldownProgress <= 0)
-			{
-				weaveCooldownTimer?.Stop();
-				weave = false;
-			}
-		}
-	}
+        public void ActivateWeaveCooldown()
+        {
+            weaveCooldownProgress = 1;
+            if (this is Player)
+            {
+                Game.UI.RunOnUIThread(() => Game.UI.UpdateWeave(weaveCooldownProgress));
+            }
+            weaveCooldownTimer?.Start();
+        }
+        protected void OnWeaveCooldownElapsed(object? sender, EventArgs e)
+        {
+            weaveCooldownProgress -= Stats.weaveCoolDownIncrement;
+            if (this is Player)
+            {
+                Game.UI.RunOnUIThread(() =>
+                {
+                    if (disposed)
+                    {
+                        return;
+                    }
+                    Game.UI.UpdateWeave(weaveCooldownProgress);
+                });
+            }
+            if (weaveCooldownProgress <= 0)
+            {
+                weaveCooldownTimer?.Stop();
+                weave = false;
+            }
+        }
+    }
 }
