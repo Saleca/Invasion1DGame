cd8ed14e8aa87190cb7ec8555be56568123aee8e
Gonçalo <70868465+Saleca@users.noreply.github.com>
2024-12-21 12:13:20 +0000

    added seed input

--- Invasion1D/Views/StartMenu.xaml.cs
+++ Invasion1D/Views/StartMenu.xaml.cs
@@ -4,6 +4,7 @@ namespace Invasion1D.Views;
 
 public partial class StartMenu : ContentPage
 {
+    int seed;
     public StartMenu()
     {
         InitializeComponent();
@@ -11,16 +12,29 @@ public partial class StartMenu : ContentPage
 
     private void Start_Clicked(object sender, EventArgs e)
     {
-        Game.Instance.Start(0, false);
+
+        Game.Instance.Start(seed);
     }
 
     private void Tutorial_Clicked(object sender, EventArgs e)
     {
-        Game.Instance.Start(0, true);
+        Game.Instance.Start(seed, true);
     }
 
     private void PrivacyPolicy_Clicked(object sender, EventArgs e)
     {
         Launcher.OpenAsync("https://saleca.github.io/Home/invasion1d/privacy-policy");
+    }
+
+    private void SeedEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (int.TryParse(e.NewTextValue, out int seed))
+        {
+            this.seed = seed;
+        }
+        else
+        {
+            SeedEntry.Text = e.OldTextValue;
+        }
     }
 }
